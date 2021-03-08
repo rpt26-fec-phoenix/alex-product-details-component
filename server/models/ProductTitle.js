18b46775d65c6297d105bb00865a6ac7aab871a1
galexy n <alexneeeeewin@icloud.com>
2021-03-07 18:53:50 -0800

    [feat] created seeding script to randomize the listing title

--- server/models/ProductTitle.js
+++ server/models/ProductTitle.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const productTitleDB = `${process.env.DB_LOCAL}/productTitle`;
-mongoose.connect(productTitleDB, {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(productTitleDB, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const productTitleSchema = new mongoose.Schema({
   'listingId': Number,
@@ -11,25 +11,63 @@ const productTitleSchema = new mongoose.Schema({
 
 const ProductTitle = mongoose.model('ProductTitle', productTitleSchema);
 
-ProductTitle.remove({}, () => console.log('cleared all rows in Product Title db'));
+if (!ProductTitle) {
+
+  const productTitleWords = process.env.PRODUCT_TITLE_WORDS.split(' ');
 
 
-const productTitleWords = process.env.PRODUCT_TITLE_WORDS.split(' ');
-console.log(`product title words split: ${JSON.stringify(productTitleWords)}`);
+  const getRandomInclusiveIntervals = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  };
 
-let newListing = new ProductTitle({
-  'listingId': 1,
-  'productListingTitle': 'Guesthouse Great Ocean & Mountain Views near Beach'
-});
+  const randomWordGenerator = (arr) => {
+    let chosenIndexes = {};
+    let randomIndex = getRandomInclusiveIntervals(0, arr.length - 1);
+    while (chosenIndexes.randomIndex) {
+      randomIndex = getRandomInclusiveIntervals(0, arr.length - 1);
+    }
+
+    chosenIndexes.randomIndex = randomIndex;
+    let randomWord = arr[randomIndex];
+    return randomWord;
+  };
+
+  const makeTitleCase = (string) => {
+    let splitString = [...string];
+    splitString[0] = splitString[0].toUpperCase();
+    return splitString.join('');
+  };
+
+  for (let i = 0; i < 100; i++) {
+
+    let productListingTitle = [];
+    let wordCount = 0;
+
+    while (wordCount < 10) {
+      productListingTitle.push(makeTitleCase(randomWordGenerator(productTitleWords).toLowerCase()));
+      wordCount++;
+    }
+
+    productListingTitle = productListingTitle.join(' ');
+
+    let newListing = new ProductTitle({
+      productListingTitle,
+      'listingId': i + 1
+    });
 
 
-newListing.save()
-  .then(response => console.log(`created new listing: ${response}`))
-  .catch(err => console.log(`error creating new listing: ${err}`));
+    newListing.save()
+      .then(response => console.log(`created new listing: ${response}`))
+      .catch(err => console.log(`error creating new listing: ${err}`));
+
+  }
+}
+
+
 
 const getProductTitleByListingId = (listingId) => {
-  return new Promise ((resolve, reject) => {
-    ProductTitle.find({ listingId: listingId}, (err, results) => {
+  return new Promise((resolve, reject) => {
+    ProductTitle.find({ listingId: listingId }, (err, results) => {
       if (err) {
         reject(err);
       } else {
